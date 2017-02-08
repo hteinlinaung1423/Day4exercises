c7a5f07835403d4dea0b6087afdecd1ca5fb5b78
hteinlinaung <lovebreakerboy.lin@gmail.com>
2017-02-08 22:30:00 +0800

    slightly changes

--- Day4exercises/exe4.cs
+++ Day4exercises/exe4.cs
@@ -18,19 +18,24 @@ namespace Day4exercises
             num = Convert.ToInt32(Console.ReadLine());
 
             Random x = new Random();
-            double g = x.Next(1, num);
+            double g = x.Next(1, num+1);
 
-            double sqrnum = Math.Round(Math.Sqrt(num),3);
+            double sqrnum = Math.Round(Math.Sqrt(num),5);
             Console.WriteLine("Random number : {0:0.000}", g);
+            Console.WriteLine("Squareroot number : {0:0.000}", sqrnum);
 
-            
 
             g *= g;
 
-            if (g == sqrnum)
+            if (g == num)
             {
                 Console.Write("Corret output : {0}", g);
             }
+            else if (num == 0)
+            {
+                Console.WriteLine("Nearest output : 0.000");
+            }
+
             else
             {
                 while (needtorepeat)
@@ -43,7 +48,7 @@ namespace Day4exercises
 
                     }
                 }
-               
+
 
             }
 
